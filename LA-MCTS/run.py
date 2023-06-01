c3d9dd9b649895fd11e7c0fb110fabcf830a4cc1
Marina Díaz Uzquiano <marina@SRFC-MARINA>
2023-06-01 19:00:39 +0200

    Add hyperparams to cli

--- LA-MCTS/run.py
+++ LA-MCTS/run.py
@@ -14,8 +14,12 @@ parser.add_argument('--dims', type=int, help='specify the problem dimensions')
 parser.add_argument('--iterations', type=int, help='specify the iterations to collect in the search')
 parser.add_argument('--bb-optimizer', type=str, help='specify the black-box optimizer to use', default='bo')
 parser.add_argument('--samples-optimizer', type=int,
-                    help='number of samples / evaluations (for bo) or max number of samples / evaluations (for turbo)',
+                    help='number of samples/evaluations (for bo) or max number of samples/evaluations (for turbo)',
                     default=None)
+parser.add_argument('--ninits', type=int, help='specify the number of random samples used in initializations')
+parser.add_argument('--cp', type=float, help='specify the Cp for MCTS')
+parser.add_argument('--leaf-size', type=int, help='specify the tree leaf size')
+parser.add_argument('--kernel-type', type=str, help='specify the kernel type')
 
 args = parser.parse_args()
 
@@ -55,11 +59,11 @@ agent = MCTS(
     lb=f.lb,  # the lower bound of each problem dimensions
     ub=f.ub,  # the upper bound of each problem dimensions
     dims=f.dims,  # the problem dimensions
-    ninits=f.ninits,  # the number of random samples used in initializations
+    ninits=f.ninits if args.ninits is None else args.ninits,  # the number of random samples used in initializations
     func=f,  # function object to be optimized
-    Cp=f.Cp,  # Cp for MCTS
-    leaf_size=f.leaf_size,  # tree leaf size
-    kernel_type=f.kernel_type,  # SVM configruation
+    Cp=f.Cp if args.cp is None else args.cp,  # Cp for MCTS
+    leaf_size=f.leaf_size if args.leaf_size is None else args.leaf_size,  # tree leaf size
+    kernel_type=f.kernel_type if args.kernel_type is None else args.kernel_type,  # SVM configruation
     gamma_type=f.gamma_type,  # SVM configruation
     solver_type=f.bb_opt,
     solver_evals=args.samples_optimizer
