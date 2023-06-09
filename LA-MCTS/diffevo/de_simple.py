Implement PSO Fix sign error in DE Go back to random sampling for DE Correct default number of evaluations for turbo Change default naming of result folders

LA-MCTS/diffevo/de_simple.py
@@ -53,13 +53,17 @@ def minimize(cost_func, lb, ub, population: np.array, mutation_factor=0.8, recom
 # --- CONSTANTS ----------------------------------------------------------------+
 
 
-def de_reproduction_sampling(population, target_index, cost_func, lb, ub, num_samples=1, mutation_factor=0.8,
+def de_reproduction_sampling(population, cost_func, lb, ub, num_samples=1, max_gen=10, mutation_factor=0.8,
                              recombination_prob=0.9):
     popsize = population.shape[0]
 
-    assert target_index in list(range(popsize))
+    population_ev = np.full(popsize, None)
+    target_index = 0
+    trial_vectors = []
+    trial_evals = []
+    generations = 1
 
-    for i in range(num_samples):
+    while len(trial_vectors) < num_samples and generations <= max_gen:
 
         # --- MUTATION (step #3.A) ---------------------+
         candidates = list(range(popsize))
@@ -84,55 +88,83 @@ def de_reproduction_sampling(population, target_index, cost_func, lb, ub, num_sa
         # --- GREEDY SELECTION (step #3.C) -------------+
 
         score_trial = cost_func(v_trial)
-        score_target = cost_func(x_t)
+        score_target = population_ev[target_index] if population_ev[target_index] is not None else cost_func(x_t)
 
         if score_trial < score_target:
             population[target_index] = v_trial
+            population_ev[target_index] = score_trial
+            print('better', score_trial, v_trial)
+        else:
+            print('worse', score_target, x_t)
+
+        trial_vectors.append(v_trial)
+        trial_evals.append(score_trial)
+
+        target_index += 1
+        if target_index >= popsize:
+            print("generation ",generations)
+            generations += 1
+            target_index = 0
+
+    return np.array(trial_vectors), np.array(trial_evals)
+
+
+def de_best_reproduction_sampling(population, cost_func, lb, ub, num_samples=1, max_gen=10, mutation_factor=0.8,
+                                  recombination_prob=0.9):
+    popsize = population.shape[0]
+
+    population_ev = [cost_func(x) for x in population]
+    target_index = 0
+    generations = 1
+
+
+    trial_vectors = []
+    trial_evals = []
+
+    while len(trial_vectors) < num_samples and generations <= max_gen:
+
+        # --- MUTATION (step #3.A) ---------------------+
+        candidates = list(range(popsize))
+        candidates.remove(target_index)
+        random_index = sample(candidates, 2)
+
+        x_1 = population[min(enumerate(population_ev), key=lambda x: x[1])[0]]
+        x_2 = population[random_index[0]]
+        x_3 = population[random_index[1]]
+        x_t = population[target_index]  # target individual
+
+        # subtract x3 from x2, and create a new vector (x_diff)
+        x_diff = x_2 - x_3
+        # multiply x_diff by the mutation factor (F) and add to x_1
+        v_donor = x_1 + mutation_factor * x_diff
+        v_donor = ensure_bounds(v_donor, lb, ub)
+
+        # --- RECOMBINATION (step #3.B) ----------------+
+
+        v_trial = np.where(np.random.random(x_t.shape) <= recombination_prob, v_donor, x_t)
+
+        # --- GREEDY SELECTION (step #3.C) -------------+
+
+        score_trial = cost_func(v_trial)
+        score_target = population_ev[target_index]
+
+        if score_trial < score_target:
+            population[target_index] = v_trial
+            population_ev[target_index] = score_trial
             print('   >', score_trial, v_trial)
         else:
             print('   >', score_target, x_t)
 
-    return v_trial, population
+        trial_vectors.append(v_trial)
+        trial_evals.append(score_trial)
 
+        target_index += 1
+        if target_index >= popsize:
+            print("generation ", generations)
+            generations += 1
+            target_index = 0
 
-def de_best_reproduction_sampling(population, target_index, cost_func, lb, ub, best_idx, mutation_factor=0.8,
-                                  recombination_prob=0.9):
-    popsize = population.shape[0]
-
-    assert target_index in list(range(popsize))
-
-    # --- MUTATION (step #3.A) ---------------------+
-    candidates = list(range(popsize))
-    candidates.remove(best_idx)
-    random_index = sample(candidates, 3)
-
-    x_1 = population[random_index[0]]
-    x_2 = population[random_index[1]]
-    x_3 = population[random_index[2]]
-    x_t = population[best_idx]  # target individual
-
-    # subtract x3 from x2, and create a new vector (x_diff)
-    x_diff = x_2 - x_3
-    # multiply x_diff by the mutation factor (F) and add to x_1
-    v_donor = x_1 + mutation_factor * x_diff
-    v_donor = ensure_bounds(v_donor, lb, ub)
-
-    # --- RECOMBINATION (step #3.B) ----------------+
-
-    v_trial = np.where(np.random.random(x_t.shape) <= recombination_prob, v_donor, x_t)
-
-    # --- GREEDY SELECTION (step #3.C) -------------+
-
-    score_trial = cost_func(v_trial)
-    score_target = cost_func(x_t)
-
-    if score_trial < score_target:
-        population[best_idx] = v_trial
-        print('   >', score_trial, v_trial)
-    else:
-        print('   >', score_target, x_t)
-
-    return v_trial, population
+    return np.array(trial_vectors), np.array(trial_evals)
 
 
 def de_sampling(population: np.array, lb, ub, mutation_factor=0.8, recombination_prob=0.9):

LA-MCTS/functions/functions.py
@@ -39,7 +39,7 @@ class tracker:
 
 
 class Levy:
-    def __init__(self, dims=10, bb_opt='bo'):
+    def __init__(self, dims=10, bb_opt=''):
         self.dims = dims
         self.lb = -10 * np.ones(dims)
         self.ub = 10 * np.ones(dims)
@@ -81,7 +81,7 @@ class Levy:
 
 
 class Ackley:
-    def __init__(self, dims=10, bb_opt='bo'):
+    def __init__(self, dims=10, bb_opt=''):
         self.dims = dims
         self.lb = -5 * np.ones(dims)
         self.ub = 10 * np.ones(dims)
@@ -109,7 +109,7 @@ class Ackley:
 
 
 class Rosenbrock:
-    def __init__(self, dims=10, bb_opt='bo'):
+    def __init__(self, dims=10, bb_opt=''):
         self.dims = dims
         self.lb = -10 * np.ones(dims)
         self.ub = 10 * np.ones(dims)

LA-MCTS/lamcts/Node.py
@@ -11,6 +11,7 @@ import operator
 import copy
 from random import sample as random_sample
 from diffevo import de_simple
+from pso import particle as pso
 
 
 class Node:
@@ -41,6 +42,11 @@ class Node:
         self.target_index = 0
         self.generation = 1
         self.best_in_gen_idx = None
+
+        # # for PSO
+        # self.particles = []
+        # self.global_best_fitness = float('inf')
+        # self.global_best_position = None
 
         self.is_svm_splittable = False
 
@@ -159,15 +165,23 @@ class Node:
         self.update_bag(samples)
 
         if de_type == 'rand':
-            proposed_X, fX = self.de_reproduction_sampling(func, num_samples=num_samples)
+            # proposed_X, fX = self.de_reproduction_sampling(func, num_samples=num_samples)
+            proposed_X, fX = self.classifier.propose_sample_de(func, path, num_samples)
         else:
-            proposed_X, fX = self.de_reproduction_sampling_best(func, num_samples=num_samples)
-
+            # proposed_X, fX = self.de_reproduction_sampling_best(func, num_samples=num_samples)
+            proposed_X, fX = self.classifier.propose_sample_de_best(func, path, num_samples)
+        fX = fX * -1
         return proposed_X, fX
 
     def propose_samples_turbo(self, num_samples, path, func):
         proposed_X, fX = self.classifier.propose_samples_turbo(num_samples, path, func)
         return proposed_X, fX
+
+    def propose_samples_pso(self, num_samples, path, func):
+        proposed_x, fx = self.classifier.propose_samples_pso(num_samples, path, func)
+        # proposed_x, fx = self.pso_sampling(func, path)
+        # fx = fx * -1
+        return proposed_x, fx
 
     def propose_samples_rand(self, num_samples):
         assert num_samples > 0
@@ -240,54 +254,54 @@ class Node:
         del self.bag[net_str]
         return json.loads(net_str)
 
-    def de_reproduction_sampling(self, func, num_samples=1, mutation_factor=0.8, recombination_prob=0.9):
-        popsize = self.population.shape[0]
-
-        assert self.target_index in list(range(popsize))
-
-        trial_vectors = []
-        trial_evals = []
-
-        for i in range(num_samples):
-
-            # --- MUTATION (step #3.A) ---------------------+
-            candidates = list(range(popsize))
-            candidates.remove(self.target_index)
-            random_index = random_sample(candidates, 3)
-
-            x_1 = self.population[random_index[0]]
-            x_2 = self.population[random_index[1]]
-            x_3 = self.population[random_index[2]]
-            x_t = self.population[self.target_index]  # target individual
-
-            # subtract x3 from x2, and create a new vector (x_diff)
-            x_diff = x_2 - x_3
-            # multiply x_diff by the mutation factor (F) and add to x_1
-            v_donor = x_1 + mutation_factor * x_diff
-            v_donor = de_simple.ensure_bounds(v_donor, func.lb, func.ub)
-
-            # --- RECOMBINATION (step #3.B) ----------------+
-
-            v_trial = np.where(np.random.random(x_t.shape) <= recombination_prob, v_donor, x_t)
-
-            # --- GREEDY SELECTION (step #3.C) -------------+
-
-            score_trial = func(v_trial)
-            score_target = self.population_ev[self.target_index]
-
-            if score_trial < score_target:
-                self.population[self.target_index] = v_trial
-                print('   >', score_trial, v_trial)
-            else:
-                print('   >', score_target, x_t)
-
-            trial_vectors.append(v_trial)
-            trial_evals.append(score_trial)
-
-            self.target_index = self.target_index + 1
-            self.check_target_index()
-
-        return trial_vectors, trial_evals
+    # def de_reproduction_sampling(self, func, num_samples=1, mutation_factor=0.8, recombination_prob=0.9):
+    #     popsize = self.population.shape[0]
+    #
+    #     assert self.target_index in list(range(popsize))
+    #
+    #     trial_vectors = []
+    #     trial_evals = []
+    #
+    #     for i in range(num_samples):
+    #
+    #         # --- MUTATION (step #3.A) ---------------------+
+    #         candidates = list(range(popsize))
+    #         candidates.remove(self.target_index)
+    #         random_index = random_sample(candidates, 3)
+    #
+    #         x_1 = self.population[random_index[0]]
+    #         x_2 = self.population[random_index[1]]
+    #         x_3 = self.population[random_index[2]]
+    #         x_t = self.population[self.target_index]  # target individual
+    #
+    #         # subtract x3 from x2, and create a new vector (x_diff)
+    #         x_diff = x_2 - x_3
+    #         # multiply x_diff by the mutation factor (F) and add to x_1
+    #         v_donor = x_1 + mutation_factor * x_diff
+    #         v_donor = de_simple.ensure_bounds(v_donor, func.lb, func.ub)
+    #
+    #         # --- RECOMBINATION (step #3.B) ----------------+
+    #
+    #         v_trial = np.where(np.random.random(x_t.shape) <= recombination_prob, v_donor, x_t)
+    #
+    #         # --- GREEDY SELECTION (step #3.C) -------------+
+    #
+    #         score_trial = func(v_trial)
+    #         score_target = self.population_ev[self.target_index]
+    #
+    #         if score_trial < score_target:
+    #             self.population[self.target_index] = v_trial
+    #             print('   >', score_trial, v_trial)
+    #         else:
+    #             print('   >', score_target, x_t)
+    #
+    #         trial_vectors.append(v_trial)
+    #         trial_evals.append(score_trial)
+    #
+    #         self.target_index = self.target_index + 1
+    #         self.check_target_index()
+    #
+    #     return np.array(trial_vectors), np.array(trial_evals)
 
     def de_reproduction_sampling_best(self, func, num_samples=1, mutation_factor=0.8, recombination_prob=0.9):
         popsize = self.population.shape[0]
@@ -326,6 +340,7 @@ class Node:
 
             if score_trial < score_target:
                 self.population[self.target_index] = v_trial
+                self.population[self.target_index] = score_trial
                 print('   >', score_trial, v_trial)
             else:
                 print('   >', score_target, x_t)
@@ -338,6 +353,50 @@ class Node:
 
         return trial_vectors, trial_evals
 
+    # def pso_sampling(self, func, path, num_iterations=10, inertia_weight=0.5, cognitive_weight=0.5,
+    #                  social_weight=0.5):
+    #
+    #     n_init = 30
+    #
+    #     if len(self.particles) == 0:
+    #         print("POPULATING PARTICLES")
+    #         samples = copy.deepcopy(self.bag)
+    #         n_bag = len(samples)
+    #         if n_bag > n_init:
+    #             samples = sorted(samples, key=lambda x: x[1])[:n_init]
+    #         elif n_bag < n_init:
+    #             n_add = n_init - n_bag
+    #             x_add = self.classifier.propose_rand_samples_sobol(n_add, path, func.lb, func.ub)
+    #             x_fx_add = [(x, func(x)*-1) for x in x_add]
+    #             samples = samples + x_fx_add
+    #
+    #         print('len(self.bag)', len(self.bag))
+    #         for sample in samples:
+    #             self.particles.append(pso.Particle(func, sample[0], sample[1] * -1))
+    #             if sample[1] * -1 < self.global_best_fitness:
+    #                 self.global_best_fitness = sample[1] * -1
+    #                 self.global_best_position = sample[0]
+    #
+    #     samples = []
+    #     sample_evals = []
+    #
+    #     for _ in range(num_iterations):
+    #         print("self.global_best_fitness", self.global_best_fitness)
+    #
+    #         for particle in self.particles:
+    #             particle.update_velocity(self.global_best_position, inertia_weight, cognitive_weight, social_weight)
+    #             particle.update_position()
+    #
+    #         for particle in self.particles:
+    #             particle.evaluate_fitness()
+    #             if particle.best_fitness < self.global_best_fitness:
+    #                 self.global_best_fitness = particle.best_fitness
+    #                 self.global_best_position = particle.best_position.copy()
+    #                 print("best fitness: ", self.global_best_fitness)
+    #                 samples.append(particle.position)
+    #                 sample_evals.append(particle.current_fitness)
+    #
+    #     return np.array(samples), np.array(sample_evals)
 # print(root)
 #
 # with open('features.json', 'r') as infile:

LA-MCTS/pso/particle.py
@@ -2,11 +2,15 @@ import numpy as np
 
 
 class Particle:
-    def __init__(self, func):
-        self.position = np.random.uniform(func.lb, func.ub, func.dims)
+    def __init__(self, func, position: np.array = None, current_fitness=None):
+        if position is None:
+            self.position = np.random.uniform(func.lb, func.ub, func.dims)
+        else:
+            self.position = position
+        self.current_fitness = current_fitness
         self.velocity = np.random.uniform(-1, 1, func.dims)
         self.best_position = self.position.copy()
-        self.best_fitness = float('inf')
+        self.best_fitness = float('inf') if current_fitness is None else current_fitness
         self.func = func
 
     def update_velocity(self, global_best_position, inertia_weight, cognitive_weight, social_weight):
@@ -22,10 +26,12 @@ class Particle:
         self.position = np.clip(self.position, self.func.lb, self.func.ub)
 
     def evaluate_fitness(self):
-        fitness = self.func(np.array(self.position))
+        fitness = self.func(self.position)
+        self.current_fitness = fitness
         if fitness < self.best_fitness:
             self.best_fitness = fitness
             self.best_position = self.position.copy()
+        return fitness
 
 
 def particle_swarm_optimization(func, num_iterations, num_particles=30, inertia_weight=0.5, cognitive_weight=0.5,
@@ -40,9 +46,37 @@ def particle_swarm_optimization(func, num_iterations, num_particles=30, inertia_
             if particle.best_fitness < global_best_fitness:
                 global_best_fitness = particle.best_fitness
                 global_best_position = particle.best_position.copy()
+                print("best fitness: ", global_best_fitness)
 
         for particle in particles:
             particle.update_velocity(global_best_position, inertia_weight, cognitive_weight, social_weight)
             particle.update_position()
 
     return global_best_position, global_best_fitness
+
+
+def pso_sampling(func, x_init, num_samples=1, max_iterations=10000, inertia_weight=0.5, cognitive_weight=0.5,
+                 social_weight=0.5):
+    particles = [Particle(func, x) for x in x_init]
+    global_best_fitness = float('inf')
+    global_best_position = None
+    samples = []
+    sample_evals = []
+    num_iterations = 0
+
+    while len(samples) < num_samples and num_iterations < max_iterations:
+        num_iterations += 1
+        for particle in particles:
+            particle.evaluate_fitness()
+            if particle.best_fitness < global_best_fitness:
+                global_best_fitness = particle.best_fitness
+                global_best_position = particle.best_position.copy()
+                print("best fitness: ", global_best_fitness)
+                samples.append(particle.position)
+                sample_evals.append(particle.current_fitness)
+
+        for particle in particles:
+            particle.update_velocity(global_best_position, inertia_weight, cognitive_weight, social_weight)
+            particle.update_position()
+
+    return np.array(samples), np.array(sample_evals)

LA-MCTS/test.py
@@ -1,56 +1,19 @@
-import random
-import numpy as np
+from pso import particle
+from functions.functions import *
 
 
-def differential_evolution(objective_func, bounds, population_size=50, max_generations=100, mutation_factor=0.8,
-                           crossover_probability=0.9):
-    num_dimensions = len(bounds)
-    population = np.zeros((population_size, num_dimensions))
+dimensions = 2
+# lower_bound = -10
+# upper_bound = 10
+num_particles = 30
+num_iterations = 640
 
-    # Initialize the population within the specified bounds
-    for i in range(population_size):
-        for j in range(num_dimensions):
-            population[i, j] = random.uniform(bounds[j][0], bounds[j][1])
+f = Rosenbrock(dims=dimensions)
 
-    # Evaluate the objective function for each individual in the population
-    fitness = np.array([objective_func(individual) for individual in population])
+best_position, best_fitness = particle.particle_swarm_optimization(f, num_iterations, num_particles)
 
-    # Main loop
-    for generation in range(max_generations):
-        for i in range(population_size):
-            target = population[i]
 
-            # Select three distinct individuals from the population
-            indices = [index for index in range(population_size) if index != i]
-            random.shuffle(indices)
-            a, b, c = population[indices[0]], population[indices[1]], population[indices[2]]
+print("Best position:", best_position)
+print("Best fitness:", best_fitness)
 
-            # Mutation: Generate a mutant individual
-            mutant = a + mutation_factor * (b - c)
 
-            # Crossover: Perform crossover between the target and the mutant
-            trial = np.copy(target)
-            j_rand = random.randint(0, num_dimensions - 1)  # Random index for crossover
-            for j in range(num_dimensions):
-                if j == j_rand or random.random() < crossover_probability:
-                    trial[j] = mutant[j]
-
-            # Ensure trial is within the specified bounds
-            trial = np.clip(trial, bounds[:, 0], bounds[:, 1])
-
-            # Selection: Replace the target with the trial if it has better fitness
-            trial_fitness = objective_func(trial)
-            if trial_fitness < fitness[i]:
-                population[i] = trial
-                fitness[i] = trial_fitness
-
-        # Print the best fitness in the current generation
-        best_fitness = np.min(fitness)
-        print(f"Generation {generation + 1}: Best Fitness = {best_fitness}")
-
-    # Return the best individual and its fitness
-    best_index = np.argmin(fitness)
-    best_individual = population[best_index]
-    best_fitness = fitness[best_index]
-
-    return best_individual, best_fitness